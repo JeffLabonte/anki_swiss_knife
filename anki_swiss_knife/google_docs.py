69c6cc4bf713cb775dad40f623d3dde41d6a9aad
Jeff Labonte <grimsleepless@protonmail.com>
2021-06-21 22:45:52 -0400

    Work on GoogleDocs

--- anki_swiss_knife/google_docs.py
+++ anki_swiss_knife/google_docs.py
@@ -10,19 +10,35 @@ class GoogleDocs:
         "https://www.googleapis.com/auth/documents",
     ]
 
-    def __init__(self):
-        self.creds = None
-        self.service = None
+    _FOLDER = "anki_swiss_knife"
 
+    def __init__(self, output_folder):
+        self._credentials = None
+        self.service = None
+        self.output_folder = Path(Path.joinpath(output_folder, self._FOLDER))
+        self.init()
+
+    def init(self):
         self._login()
+        self._create_folder()
+
+    def _create_folder(self):
+        if not os.path.exists(self.output_folder):
+            os.mkdir(path=self.output_folder)
 
     def _login(self):
         filepath = Path(Path.joinpath(Path.home(), ".config", "anki_swiss_tool", "credentials.json"))
         if filepath.exists():
-            self.creds = Credentials.from_service_account_file(filename=filepath, scopes=self.SCOPES)
-            self.service = build("docs", "v1", credentials=self.creds)
+            self._credentials = Credentials.from_service_account_file(filename=filepath, scopes=self.SCOPES)
+            self.service = build("docs", "v1", credentials=self._credentials)
         else:
             raise FileNotFoundError()
 
     def get_document(self, document_id: str):
         return self.service.documents().get(documentId=document_id)
+
+    def extract_document_to_file(self, document_id):
+        document = self.get_document(document_id=document_id).execute()
+        document_content = document["body"]["content"]
+        with open("", "w+") as f:
+            print("Test")
