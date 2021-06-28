88655d138e44e68be9f37b52da8cdcf342ed8e0c
Jeff Labonte <grimsleepless@protonmail.com>
2021-06-27 23:07:34 -0400

    Update most of the generator to format the file

--- anki_swiss_knife/csv_generator.py
+++ anki_swiss_knife/csv_generator.py
@@ -3,10 +3,14 @@ from typing import List, Optional, Tuple
 
 
 class CSVGenerator:
-    ADD_RULES = [
-        " + V",
-        "+V"
-    ]
+    TEXT_TO_KEEP_IN_FIRST_COLUMN = (
+        " + V.",
+        "+V.",
+    )
+
+    TEXT_TO_REMOVE = (
+        "(future tense)",
+    )
 
     def __init__(self, file_to_convert: str, extra_rules: Optional[List[str]] = None):
         self.file_to_convert = file_to_convert
@@ -18,11 +22,39 @@ class CSVGenerator:
             is_chinese = not is_chinese
         return is_chinese
 
-    def generate_row(self, line: str) -> Tuple:
-        content_words = line.split(" ")
-        rest_of_sentense = " ".join(content_words[1:])
-        chinese_chars = content_words[0].strip(" ")
-        return chinese_chars, rest_of_sentense
+    def _remove_unwanted_text(self, text: str) -> str:
+        text = text.lstrip(" ")
+        for remove in self.TEXT_TO_REMOVE:
+            text = text.replace(remove, "").lstrip(" ")
+        return text
+
+    def _find_end_index_for_text_to_keep(self, text: str) -> List[int]:
+        return [
+            text.index(text_to_keep) + len(text_to_keep)
+            for text_to_keep in self.TEXT_TO_KEEP_IN_FIRST_COLUMN
+            if text_to_keep in text
+        ]
+
+    def _find_end_index_for_chinese_char(self, text: str) -> int:
+        for index, character in enumerate(text):
+            if not self._is_chinese_character(character=character):
+                return index
+
+    def find_first_column_ends(self, text: str) -> int:
+        indexes = self._find_end_index_for_text_to_keep(text=text)
+        if indexes:
+            indexes.sort(reverse=True)
+            return indexes[0]
+        else:
+            return self._find_end_index_for_chinese_char(text=text)
+
+    def generate_row(self, line: str) -> str:
+        index = self.find_first_column_ends(text=line)
+
+        chinese_chars = self._remove_unwanted_text(text=line[0:index])
+        rest_of_sentence = self._remove_unwanted_text(text=line[index:])
+
+        return f"{chinese_chars};{rest_of_sentence}"
 
     def generate_csv(self):
         file_content = self._read_file()
