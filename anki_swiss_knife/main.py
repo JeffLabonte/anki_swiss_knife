5a713064a49fce7671a6f97cba7eb5c236aa3e0a
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-05 11:50:27 -0400

    Rename class for something more explicit

--- anki_swiss_knife/main.py
+++ anki_swiss_knife/main.py
@@ -2,7 +2,7 @@ import argparse
 from pathlib import Path
 
 from anki_swiss_knife.chinese_csv_generator import ChineseCSVGenerator
-from anki_swiss_knife.google_docs import GoogleDocs
+from anki_swiss_knife.google_docs_document_reader import GoogleDocsDocumentReader
 
 parser = argparse.ArgumentParser(description="What document do you want to parse")
 
@@ -27,6 +27,6 @@ if __name__ == "__main__":
     document_id = args.document_id
 
     print(f"[+] Extracting Google Docs Document ID: {document_id}")
-    google_docs = GoogleDocs(output_folder=args.output_folder)
+    google_docs = GoogleDocsDocumentReader(output_folder=args.output_folder)
     filepath = google_docs.extract_document_to_file(document_id=document_id)
     ChineseCSVGenerator(file_to_convert=filepath).generate_csv()
