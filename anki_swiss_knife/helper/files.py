8c7826094579cd54cda9e2bb2c3e489c6240563c
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-13 17:56:08 -0400

    Add helper to read_file and add signature to create_folder

--- anki_swiss_knife/helper/files.py
+++ anki_swiss_knife/helper/files.py
@@ -1,6 +1,12 @@
 import os
+from typing import Generator
 
 
-def create_folder(file_path: str):
+def create_folder(file_path: str) -> None:
     if not os.path.exists(file_path):
         os.mkdir(file_path)
+
+
+def read_file(file_path: str) -> Generator:
+    with open(file_path, "r") as file:
+        return (line for line in file.readlines())
