cdcd313c75245ae0375e77a987895a16cd8771a1
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-14 20:56:44 -0400

    Create write_csv and update delimiter string to use CSV_DELEMITER

--- anki_swiss_knife/helper/csv.py
+++ anki_swiss_knife/helper/csv.py
@@ -1,4 +1,7 @@
 import csv
+from typing import List
+
+from anki_swiss_knife.constants import base
 
 
 def read_csv(file_path):
@@ -6,8 +9,13 @@ def read_csv(file_path):
         raise RuntimeError("File extension is not .csv")
 
     with open(file_path, "r") as csv_file:
-        return csv.reader(csvfile=csv_file, delimiter=";")
+        return csv.reader(csvfile=csv_file, delimiter=base.CSV_DELIMITER)
 
 
-def write_csv(content, file_path):
-    pass
+def write_csv(contents: List, file_path: str) -> None:
+    csv_writer = csv.writer(csvfile=file_path, delimiter=base.CSV_DELIMITER)
+    for content in contents:
+        if getattr(content, "to_list"):
+            csv_writer.writerow(content.to_list())
+        else:
+            csv_writer.writerow(content.split(base.CSV_DELIMITER))
