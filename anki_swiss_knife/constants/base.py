e6dcb3937db902188feebe149db12815a84ffcdd
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-15 22:27:53 -0400

    Add DEFAULT_ANKI_USER and COLLECTION_MEDIA_LINUX_PATH to base constants

--- anki_swiss_knife/constants/base.py
+++ anki_swiss_knife/constants/base.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
 PROJECT_NAME = "anki-swiss-knife"
 
 CSV_DELIMITER = ";"
+
+DEFAULT_ANKI_USER = "User 1"
+
+COLLECTION_MEDIA_LINUX_PATH = os.path.join(Path.home(), ".local/share/Anki1/{anki_user}/collection.media/")
