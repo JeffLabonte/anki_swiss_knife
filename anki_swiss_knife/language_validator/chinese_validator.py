db977dba568a6e66d16b55bad8412659b70c4c7d
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-17 02:51:11 -0400

    Add _is_character in unicode

--- anki_swiss_knife/language_validator/chinese_validator.py
+++ anki_swiss_knife/language_validator/chinese_validator.py
@@ -1,6 +1,24 @@
+import re
+
+
 class ChineseValidator:
     CHINESE_UNICODES = "[\u4e00-\u9FFF]"
     LATIN_UNICODES = "[\u0000-\u007F]"
 
+    NOT_FOUND_RE_SEARCH = 0
+
     def __init__(self):
         pass
+
+    def _is_character_in_unicode(self, unicode_regex: str, character: str):
+        return (
+            getattr(
+                re.search(
+                    unicode_regex,
+                    character,
+                ),
+                "endpos",
+                self.NOT_FOUND_RE_SEARCH,
+            )
+            != self.NOT_FOUND_RE_SEARCH
+        )
