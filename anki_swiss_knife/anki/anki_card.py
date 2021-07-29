5870da20ac6c42af0a4f942b8ef580eb8f725c6c
Jeff Labonte <grimsleepless@protonmail.com>
2021-07-28 23:02:27 -0400

    Create ChineseAnkiCard with to_csv

---
+++ anki_swiss_knife/anki/anki_card.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ChineseAnkiCard:
+    chinese_character: str
+    pinyin: str
+    translation: str
+
+    def to_csv(self, is_chinese_first: bool) -> str:
+        first_column = self.chinese_character if is_chinese_first else self.translation
+        following_pinyin = self.translation if is_chinese_first else self.chinese_character
+
+        second_column = f"{self.pinyin.rstrip()}\n{following_pinyin.lstrip()}"
+        return ";".join([first_column, second_column])
