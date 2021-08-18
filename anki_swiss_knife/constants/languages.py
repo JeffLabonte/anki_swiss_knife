2ecbf1b3c3f7643bbcbbce5586be55e9f106e752
Jeff Labonte <grimsleepless@protonmail.com>
2021-08-17 22:17:08 -0400

    Restructure the code to be easier to follow

--- anki_swiss_knife/constants/languages.py
+++ anki_swiss_knife/constants/languages.py
@@ -5,3 +5,27 @@ CHINESE_UNICODES = "[\u4e00-\u9FFF]"
 LATIN_UNICODES = "[\u0000-\u007F]"
 
 CHINESE_TEXT_TO_REMOVE = ("(future tense)",)
+
+CHINESE_WORDS_TO_KEEP = (
+    " + V.",
+    "+ V.",
+    "+V.",
+    " + measure word",
+    "+ measure word",
+    "+measure word",
+    "SF",
+    "Quebec City",
+    " + W",
+    "V. + ",
+    "(个)",
+    "Harry Potter",
+    "Star Wars",
+    "+someone.+",
+    "Ajd. / V.  + ",
+    "(是)",
+    "(v./n.)",
+    "Harry Potter",
+    "SF",
+    "Quebec City",
+    "Star Wars",
+)
--- anki_swiss_knife/language_extractors.py
+++ anki_swiss_knife/language_extractors.py
@@ -1,14 +1,37 @@
-from typing import Tuple
-from anki_swiss_knife.language_validator.chinese_characters_validator import ChineseCharacterValidator
 from functools import lru_cache
+from typing import List, Tuple
 
+from anki_swiss_knife.language_validator.chinese_characters_validator import ChineseCharacterValidator
 
 chinese_validator = ChineseCharacterValidator()
 
 
+def get_indexes_of_words_to_keep_in_phrase(
+    phrase: str,
+    words_to_keep: Tuple[str],
+) -> Tuple[Tuple[str]]:
+    list_of_index = []
+    for name in words_to_keep:
+        start_index = phrase.index(name)
+        end_index = start_index + len(name)
+        list_of_index.append(
+            (
+                start_index,
+                end_index,
+            )
+        )
+    return tuple(list_of_index)
+
+
 @lru_cache()
-def get_last_chinese_character_index(phrase: str) -> int:
+def get_last_chinese_character_index(phrase: str, words_to_keep: List[str] = []) -> int:
+    start_end_index = get_indexes_of_words_to_keep_in_phrase(phrase=phrase, words_to_keep=words_to_keep)
     for index, character in enumerate(phrase):
+        if isinstance(start_end_index, Tuple):
+            start, end = start_end_index
+            if start >= index >= end:
+                continue
+
         is_chinese = chinese_validator.is_chinese_character(character=character)
         if not is_chinese:
             return index
