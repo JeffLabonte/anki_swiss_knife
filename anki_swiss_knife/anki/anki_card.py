0ec560ba10582536b3b2ffdb1dd1db8504a65d82
Jeff Labonte <grimsleepless@protonmail.com>
2021-08-22 22:11:02 -0400

    Fix the CSV

--- anki_swiss_knife/anki/anki_card.py
+++ anki_swiss_knife/anki/anki_card.py
@@ -12,6 +12,6 @@ class ChineseAnkiCard:
         first_column = self.chinese_character if is_chinese_first else self.translation
         following_pinyin = self.translation if is_chinese_first else self.chinese_character
 
-        second_column = f"{self.pinyin.rstrip()}\n{following_pinyin.lstrip()}"
-        csv = ";".join([first_column, second_column, self.notes])
+        second_column = f"{self.pinyin.rstrip()} {following_pinyin.lstrip()}, {self.notes}"
+        csv = ";".join([first_column, second_column])
         return f"{csv}\n"
