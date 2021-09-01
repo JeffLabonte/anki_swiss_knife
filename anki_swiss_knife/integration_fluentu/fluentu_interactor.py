fad7f0231f16cd76daf03a5aa568f866a3b0c051
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-01 11:20:59 -0400

    Update FluentInteractor to open firefox in headless

--- anki_swiss_knife/integration_fluentu/fluentu_interactor.py
+++ anki_swiss_knife/integration_fluentu/fluentu_interactor.py
@@ -1,6 +1,15 @@
+import os
+
 from selenium import webdriver
 
 
 class FluentuInteractor:
+    FLUENTU_LOGIN_URL = "https://www.fluentu.com/login"
+
     def __init__(self):
+        os.environ["MOZ_HEADLESS"] = "1"
         self.driver = webdriver.Firefox()
+        self.driver.get(self.FLUENTU_LOGIN_URL)
+
+    def __del__(self):
+        self.driver.close()
