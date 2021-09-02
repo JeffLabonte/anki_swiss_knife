87fad8ef99eb06518ab9149ae193bcc4a8d74038
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-02 12:22:16 -0400

    Make test more restrictive

--- tests/test_integration_fluentu/test_fluentu_interactor.py
+++ tests/test_integration_fluentu/test_fluentu_interactor.py
@@ -1,6 +1,8 @@
 from anki_swiss_knife.integration_fluentu.fluentu_interactor import FluentuInteractor
 
 
-def test__fluentu_interator__init__should_open_login_page():
+def test__fluentu_interactor__init__should_open_login_page():
     fluentu_interactor = FluentuInteractor()
     assert fluentu_interactor.driver.title == "FluentU"
+    assert fluentu_interactor.driver.find_element_by_id("login")
+    assert fluentu_interactor.driver.find_element_by_id("password")
