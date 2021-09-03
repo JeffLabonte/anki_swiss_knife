2d23cd6d330d848a657170e195a40e9f043ec9c5
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-02 21:59:22 -0400

    First iteration on credential helper

---
+++ anki_swiss_knife/helper/credentials.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+
+def get_credentials(service: str) -> Dict:
+    pass
+
+
+def decrypt_password(encrypted_password) -> str:
+    """
+    Decrypt Message from get_credentials
+    """
+    pass
