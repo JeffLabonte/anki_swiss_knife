10e68393fb5f3826d0234f41f376891c3d688ec6
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-02 20:56:18 -0400

    Add black fix

--- tests/test_helper/test_files.py
+++ tests/test_helper/test_files.py
@@ -1,7 +1,7 @@
 import pytest
 
 from anki_swiss_knife.helper.files import create_initial_file
-from anki_swiss_knife.constants import file_paths
+from anki_swiss_knife.constants import base
 
 
 def test__files__create_file_not_supported__should_raise_not_implemented():
@@ -13,4 +13,7 @@ def test__files__create_file_not_supported__should_raise_not_implemented():
 
 
 def test__files__create_init_file__should_not_raise_with__valid_format():
-    create_initial_file(file_path=file_paths.CONFIGURATION_FILE, content={"a": "dev"},)
+    create_initial_file(
+        file_path=base.CONFIGURATION_FILE_INI,
+        content={"a": "dev"},
+    )
