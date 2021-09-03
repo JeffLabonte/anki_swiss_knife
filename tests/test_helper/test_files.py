24ae52f3579fc4a0b70c703bb825fd66504f5bee
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-02 21:37:53 -0400

    Add additional tests

--- tests/test_helper/test_files.py
+++ tests/test_helper/test_files.py
@@ -1,10 +1,12 @@
+from unittest.mock import MagicMock, patch
+
 import pytest
 
-from anki_swiss_knife.helper.files import create_initial_file
 from anki_swiss_knife.constants import base
+from anki_swiss_knife.helper.files import create_initial_file
 
 
-def test__files__create_file_not_supported__should_raise_not_implemented():
+def test__files__create_initial_file_not_supported__should_raise_not_implemented():
     with pytest.raises(NotImplementedError):
         create_initial_file(
             file_path="/home/jflabonte/.config/test",
@@ -12,8 +14,29 @@ def test__files__create_file_not_supported__should_raise_not_implemented():
         )
 
 
-def test__files__create_init_file__should_not_raise_with__valid_format():
+def test__files__create_initial_file__should_not_raise_with__valid_format():
     create_initial_file(
         file_path=base.CONFIGURATION_FILE_INI,
         content={"a": "dev"},
     )
+
+
+def test__files_create_initial_file__should_call_when_file_doesnt_exists_supported_extension():
+    from anki_swiss_knife.helper.files import SUPPORTED_EXTENSIONS
+
+    mocked_function = MagicMock()
+    with patch.dict(SUPPORTED_EXTENSIONS, {"ini": mocked_function}), patch(
+        "anki_swiss_knife.helper.files.os.path.exists", return_value=False
+    ) as exists_mock:
+        create_initial_file(
+            file_path=base.CONFIGURATION_FILE_INI,
+            content={"a": "dev"},
+        )
+        assert mocked_function.called
+        assert exists_mock.called
+
+        mocked_function.assert_called_once_with(
+            file_path=base.CONFIGURATION_FILE_INI,
+            content={"a": "dev"},
+        )
+        exists_mock.assert_called_once_with(base.CONFIGURATION_FILE_INI)
