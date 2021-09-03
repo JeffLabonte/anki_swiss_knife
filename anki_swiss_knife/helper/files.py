3c7cfa52f2e00143b31bec2c6080d70173f58c40
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-02 21:37:24 -0400

    Refactor the function to create initial file

--- anki_swiss_knife/helper/files.py
+++ anki_swiss_knife/helper/files.py
@@ -1,8 +1,11 @@
+import os
 from pathlib import Path
-from typing import Dict, Generator
+from typing import Dict, Generator, Optional
+
+from anki_swiss_knife.helper.ini_file import create_ini_file
 
 SUPPORTED_EXTENSIONS = {
-    ".ini",
+    "ini": create_ini_file,
 }
 
 
@@ -10,11 +13,23 @@ def create_folder(file_path: str) -> None:
     Path(file_path).mkdir(parents=True, exist_ok=True)
 
 
-def create_initial_file(file_path: str, content: Dict):
-    if not any(file_path.endswith(format) for format in SUPPORTED_EXTENSIONS):
+def get_file_extension(file_path: str) -> Optional[str]:
+    if file_extension := file_path.split("."):
+        return file_extension[-1]
+
+
+def create_initial_file(file_path: str, content: Dict, force: bool = False):
+    file_extension = get_file_extension(file_path=file_path)
+    if not any(file_extension == extension for extension in SUPPORTED_EXTENSIONS.keys()):
         raise NotImplementedError(
             f"[X] The file path extension: {file_path} is not supported.",
             f"[X] This function supports: {SUPPORTED_EXTENSIONS}",
+        )
+
+    if force or not os.path.exists(file_path):
+        SUPPORTED_EXTENSIONS[file_extension](
+            file_path=file_path,
+            content=content,
         )
 
 
