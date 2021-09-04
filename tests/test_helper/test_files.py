4e0cf2c077806d91502d85970f29438c6dd13863
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-03 23:27:45 -0400

    Create a setup with a namedtuple

--- tests/test_helper/test_files.py
+++ tests/test_helper/test_files.py
@@ -1,9 +1,39 @@
+from collections import namedtuple
 from unittest.mock import MagicMock, patch
 
 import pytest
 
 from anki_swiss_knife.constants import base
 from anki_swiss_knife.helper.files import create_initial_file
+
+CreateInitialFileMocks = namedtuple(
+    "CreateInitialFileMocks",
+    [
+        "path_exists_mock",
+        "mocked_function",
+    ],
+)
+
+
+def setup_create_initial_file(
+    file_exists: bool,
+    force_create: bool = False,
+) -> CreateInitialFileMocks:
+    from anki_swiss_knife.helper.files import SUPPORTED_EXTENSIONS
+
+    mocked_function = MagicMock()
+    with patch.dict(SUPPORTED_EXTENSIONS, {"ini": mocked_function}), patch(
+        "anki_swiss_knife.helper.files.os.path.exists", return_value=file_exists
+    ) as exists_mock:
+        create_initial_file(
+            file_path=base.CONFIGURATION_FILE_INI,
+            content={"a": "dev"},
+            force=force_create,
+        )
+        return CreateInitialFileMocks(
+            path_exists_mock=exists_mock,
+            mocked_function=mocked_function,
+        )
 
 
 def test__files__create_initial_file_not_supported__should_raise_not_implemented():
