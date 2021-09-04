bdf2d13bf1c290cc052ba95f36bcd114f8d662a9
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-03 23:29:47 -0400

    Update test

--- tests/test_helper/test_files.py
+++ tests/test_helper/test_files.py
@@ -51,22 +51,15 @@ def test__files__create_initial_file__should_not_raise_with__valid_format():
     )
 
 
-def test__files_create_initial_file__should_call_when_file_doesnt_exists_supported_extension():
-    from anki_swiss_knife.helper.files import SUPPORTED_EXTENSIONS
+def test__files_create_initial_file__should_call_function_when_file_doesnt_exists_supported_extension():
+    mocks = setup_create_initial_file(file_exists=False)
 
-    mocked_function = MagicMock()
-    with patch.dict(SUPPORTED_EXTENSIONS, {"ini": mocked_function}), patch(
-        "anki_swiss_knife.helper.files.os.path.exists", return_value=False
-    ) as exists_mock:
-        create_initial_file(
-            file_path=base.CONFIGURATION_FILE_INI,
-            content={"a": "dev"},
-        )
-        assert mocked_function.called
-        assert exists_mock.called
+    assert mocks.mocked_function.called
+    assert mocks.path_exists_mock.called
 
-        mocked_function.assert_called_once_with(
-            file_path=base.CONFIGURATION_FILE_INI,
-            content={"a": "dev"},
-        )
-        exists_mock.assert_called_once_with(base.CONFIGURATION_FILE_INI)
+    mocks.mocked_function.assert_called_once_with(
+        file_path=base.CONFIGURATION_FILE_INI,
+        content={"a": "dev"},
+    )
+    mocks.path_exists_mock.assert_called_once_with(base.CONFIGURATION_FILE_INI)
+
