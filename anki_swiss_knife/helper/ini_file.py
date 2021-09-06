d1a0247f179a80cd4b19de1db108f7eabaa63cb3
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-06 00:24:36 -0400

    Create function to write a config file from a Dict

--- anki_swiss_knife/helper/ini_file.py
+++ anki_swiss_knife/helper/ini_file.py
@@ -1,2 +1,31 @@
-def create_ini_file(file_path: str, content: str):
-    pass
+import configparser
+from typing import Dict
+
+
+def create_ini_file(file_path: str, content: Dict) -> None:
+    """
+    Expected format for content:
+
+    ```json
+    {
+        "DEFAULT": {
+            "config_a": 1
+        },
+        "GROUP_A": {
+            "config_b": "valueB"
+        }
+    }
+    ```
+    result:
+    [DEFAULT]
+    config_a = 1
+
+    [GROUP_A]
+    config_b = valueB
+    """
+    config = configparser.ConfigParser()
+    for key, value in content:
+        config[key] = str(value)
+
+    with open(file_path, "w+") as init_file:
+        config.write(init_file)
