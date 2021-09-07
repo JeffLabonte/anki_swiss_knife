9ca7d95d42945b3f1c2d4dc727cf084255ebbb26
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-06 22:38:12 -0400

    Fix path to get credentials

--- anki_swiss_knife/constants/configs.py
+++ anki_swiss_knife/constants/configs.py
@@ -6,7 +6,6 @@ from anki_swiss_knife.constants import base
 CONFIGURATION_FOLDER = os.path.join(Path.home(), f".config/{base.PROJECT_NAME}")
 CONFIGURATION_FILE_INI = os.path.join(Path.home(), f".config/{base.PROJECT_NAME}/default.ini")
 
-
 DEFAULT_CONFIGURATIONS = {
     "General": {
         "text-with-speech": True,
--- anki_swiss_knife/google_docs_document_reader.py
+++ anki_swiss_knife/google_docs_document_reader.py
@@ -56,7 +56,7 @@ class GoogleDocsDocumentReader:
             Path.joinpath(
                 Path.home(),
                 ".config",
-                "anki_swiss_tool",
+                "anki_swiss_knife",
                 "credentials.json",
             )
         )
@@ -87,7 +87,7 @@ class GoogleDocsDocumentReader:
         vocabulary_start_index = self.find_page_flag(contents=contents)
         saved_file_path = os.path.join(self.output_folder, f"{document['title']}.txt")
         with open(saved_file_path, "w+") as f:
-            f.writelines(contents[vocabulary_start_index + 1 :])
+            f.writelines(contents[vocabulary_start_index + 1:])
             print(f"[+] File saved: {saved_file_path}")
             return saved_file_path
 
