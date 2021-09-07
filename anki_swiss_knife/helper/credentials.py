03942a345821c0cbd8c096cef1cffdcbf0367d60
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-06 22:45:32 -0400

    Add typing to decrypt_password

--- anki_swiss_knife/helper/credentials.py
+++ anki_swiss_knife/helper/credentials.py
@@ -5,7 +5,7 @@ def get_credentials(service: str) -> Dict:
     pass
 
 
-def decrypt_password(encrypted_password) -> str:
+def decrypt_password(encrypted_password: str) -> str:
     """
     Decrypt Message from get_credentials
     """
