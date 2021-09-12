5de3a913870537ecdd84845bc7f54e443b2eddc6
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-12 19:38:35 +0000

    Update fixture to create initial configuration file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,11 @@ import os
 import pytest
 
 from anki_swiss_knife.constants.configs import CONFIGURATION_FILE_INI
+from anki_swiss_knife.main import initial_startup
 
 
 @pytest.fixture
 def fake_configs(fs):
     fs.create_file(CONFIGURATION_FILE_INI)
-    assert os.path.exists(CONFIGURATION_FILE_INI)
-    
+    initial_startup()
     yield fs
