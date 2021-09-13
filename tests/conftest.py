f6361a7217219bd8dc527b9024dd5bb5fbbd4cd1
Jeff Labonte <grimsleepless@protonmail.com>
2021-09-13 00:21:18 +0000

    WIP - Create new test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,11 @@
-import os
-
 import pytest
 
 from anki_swiss_knife.constants.configs import CONFIGURATION_FILE_INI
 from anki_swiss_knife.main import initial_startup
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def fake_configs(fs):
     fs.create_file(CONFIGURATION_FILE_INI)
-    initial_startup()
+    initial_startup(force=True)
     yield fs
--- tests/test_helper/test_configs.py
+++ tests/test_helper/test_configs.py
@@ -1,8 +1,14 @@
 from anki_swiss_knife.constants import configs as configs_constant
-from anki_swiss_knife.helper import configs
+from anki_swiss_knife.helper import configs as configs_helper
 
 
 def test__helper_configs__read_configs__should_return_dict_with_configs(fake_configs):
-    config_dict = configs.read_config(file_path=configs_constant.CONFIGURATION_FILE_INI)
+    config_dict = configs_helper.read_config(file_path=configs_constant.CONFIGURATION_FILE_INI)
 
     assert isinstance(config_dict, dict)
+
+    for section, configs in configs_constant.DEFAULT_CONFIGURATIONS.items():
+        config_section = config_dict.pop(section)
+        for config, value in configs.items():
+            if config_section:
+                assert config_section.pop(config, False) == value, f"Value for config: {config} doesn't match"
